a1f5e2914e33cb15e37f0ee5dd0817eed0fc4caf
MaksShizo <79019923+MaksShizo@users.noreply.github.com>
2021-05-29 22:22:07 +0500

    Add files via upload

---
+++ Lab_18.9.1.cpp
@@ -0,0 +1,38 @@
+﻿#include "Vector.h"
+#include <iostream>
+using namespace std;
+int main()
+{
+	
+	setlocale(LC_ALL, "rus");
+	int	mass[3] = { 2,1,3 };
+		try
+		{
+			Vector x(3, mass);
+			cout << "Элементы группы: ";
+			cout << x;
+			cout << "Введите индекс\n";
+			int i;
+			cin >> i;
+			cout << "Элемент : ";
+			cout << x[i] << endl;
+			cout << "Вычитаем \n";
+			--x;
+			cout << x;
+			cout << "Вычитаем \n";
+			--x;
+			cout << x;
+			cout << "Вычитаем \n";
+			--x;
+			cout << x;
+			cout << "Вычитаем \n";
+			--x;
+			cout << x;
+		}
+		catch (int)
+		{
+			cout << "Ошибка" << endl;
+		}
+		return 0;
+	}
+
